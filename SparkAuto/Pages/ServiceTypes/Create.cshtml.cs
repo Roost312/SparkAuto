185c5dd61de57eb7a9bc1dfd81e2fab0a43bbfc9
Jessen Gilbert <jessen.gilbert@yahoo.com>
2019-09-16 13:28:26 -0600

    Monday work done in class

---
+++ SparkAuto/Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        //Post - create
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //the bindproperty defines the servicetype being added
+            _db.ServiceType.Add(ServiceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
+        }
+
+    }
+}
---
+++ SparkAuto/Pages/ServiceTypes/Edit.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class EditModel : PageModel
+    {
+        private readonly SparkAuto.Data.ApplicationDbContext _db;
+
+        public EditModel(SparkAuto.Data.ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _db.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ServiceType serviceFromDb = await _db.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+            serviceFromDb.Name = ServiceType.Name;
+            serviceFromDb.Price = ServiceType.Price;
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
