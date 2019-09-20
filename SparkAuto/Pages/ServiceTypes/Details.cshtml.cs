c87bb6c16c5c39ec0ea1728e350d5695f95d6650
Jessen Gilbert <jessen.gilbert@yahoo.com>
2019-09-20 14:46:44 -0600

    Made corrections for icons, tried to rename published site but still shows as BookList. Remind myself to talk to Professor about Icons

---
+++ SparkAuto/Pages/ServiceTypes/Details.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DetailsModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGet(int id)
+        {
+            ServiceType = await _db.ServiceType.FirstOrDefaultAsync(m => m.Id == id);
+
+            return Page();
+        }
+    }
+}
